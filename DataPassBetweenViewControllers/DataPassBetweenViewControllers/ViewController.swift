1ac85cdc6c2d9d15248ecac4c886066d21fe53b0
shuvokr <shuvo@Shuvos-Air.mshome.net>
2019-09-29 01:33:19 +0600

    Created new project - Data Pass Between ViewController's

---
+++ DataPassBetweenViewControllers/DataPassBetweenViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DataPassBetweenViewControllers
+//
+//  Created by Shuvo on 29/9/19.
+//  Copyright © 2019 com.example.learn.iOS.swift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
